58673c05fa84098d25d99488fe4909e583bb3384
indi-c <indigoconnelly@pm.me>
2026-07-24 03:23:15 +0100

    changed styling

--- genpages.py
+++ genpages.py
@@ -25,9 +25,6 @@ title: Free Learning - {title}
 ---
 '''
 
-color_one = "#fbfbff"
-color_two = "#EBF1F7"
-
 # make a new file for each domain in _learning/ by the name of the domain id 
 # by using the html_template and filling in the title with the domain title
 # append the freelearningcontent include for each field in the domain to the file, 
@@ -35,68 +32,58 @@ color_two = "#EBF1F7"
 # also make a new folder for each domain in _learning/domains/ by the name of the domain id
 for domain in domains:
     domain_id = domain['id']
-    domain_title = domain['title'].title()
+    domain_title = domain['title'].title() if domain['title'] != domain['title'].upper() else domain['title']
     domain_fields = domain_to_fields[domain_id]
-
-    current_color = color_one
 
     # make a new folder for each domain in _learning/ by the name of the domain id
     os.makedirs(f'_learning/{domain_id}', exist_ok=True)
 
     with open(f'_learning/{domain_id}.html', 'w') as f:
         f.write(html_template.format(title=domain_title))
+        f.write(f'{{% include freelearningtopictitle.html url="{domain_id}" title="{domain_title}" description="{domain["description"]}" %}}\n')
 
         for field in domain_fields:
             field_id = field['id']
-            field_title = field['title'].title()
+            field_title = field['title'].title() if field['title'] != field['title'].upper() else field['title']
             field_brief = field['brief']
-            f.write(f'{{% include freelearningcontent.html url="{field_id}" title="{field_title}" brief="{field_brief}" color="{current_color}" %}}\n')
-            current_color = color_two if current_color == color_one else color_one
+            f.write(f'{{% include freelearningcontent.html url="{field_id}" title="{field_title}" brief="{field_brief}" %}}\n')
 
 # repeat the above for each field in _learning/fields/ by the name of the field id
 for field in fields:
     field_id = field['id']
     field_domain = field['domain']
-    field_title = field['title'].title()
+    field_title = field['title'].title() if field['title'] != field['title'].upper() else field['title']
     field_description = field['description']
     field_topics = field_to_topics[field_id]
-
-    current_color = color_one
 
     # make a new folder for each field in _learning/{domain} by the name of the field id
     os.makedirs(f'_learning/{field_domain}/{field_id}', exist_ok=True)
 
     with open(f'_learning/{field_domain}/{field_id}.html', 'w') as f:
         f.write(html_template.format(title=field_title))
-        f.write(f'{{% include freelearningcontent.html url="{field_id}" title="{field_title}" brief="{field_description}" color="{current_color}" %}}\n')
-        current_color = color_two if current_color == color_one else color_one
+        f.write(f'{{% include freelearningtopictitle.html url="{field_id}" title="{field_title}" description="{field_description}" %}}\n')
         for topic in field_topics:
             topic_id = topic['id']
-            topic_title = topic['title'].title()
+            topic_title = topic['title'].title() if topic['title'] != topic['title'].upper() else topic['title']
             topic_brief = topic['brief']
-            f.write(f'{{% include freelearningcontent.html url="{topic_id}" title="{topic_title}" brief="{topic_brief}" color="{current_color}" %}}\n')
-            current_color = color_two if current_color == color_one else color_one
+            f.write(f'{{% include freelearningcontent.html url="{topic_id}" title="{topic_title}" brief="{topic_brief}" %}}\n')
 
 # repeat the above for each topic in _learning/topics/ by the name of the topic id but populating with information from resources.yml
 resources = yaml.safe_load(open('_data/freelearning/resources.yml'))
 for topic in topics:
     topic_id = topic['id']
     topic_field = topic['field']
-    topic_title = topic['title'].title()
+    topic_title = topic['title'].title() if topic['title'] != topic['title'].upper() else topic['title']
     topic_description = topic['description']
     topic_resources = [resource for resource in resources if topic_id in resource['topics']]
     topic_domain = next((field['domain'] for field in fields if field['id'] == topic_field), None)
 
-    current_color = color_one
-
     with open(f'_learning/{topic_domain}/{topic_field}/{topic_id}.html', 'w') as f:
         f.write(html_template.format(title=topic_title))
-        f.write(f'{{% include freelearningcontent.html url="{topic_id}" title="{topic_title}" brief="{topic_description}" color="{current_color}" %}}\n')
-        current_color = color_two if current_color == color_one else color_one
+        f.write(f'{{% include freelearningtopictitle.html url="{topic_id}" title="{topic_title}" description="{topic_description}" %}}\n')
         for resource in topic_resources:
             resource_id = resource['id']
-            resource_title = resource['title'].title()
+            resource_title = resource['title'].title() if resource['title'] != resource['title'].upper() else resource['title']
             resource_brief = resource['brief']
             resource_url = resource['url']
-            f.write(f'{{% include freelearningcontent.html url="{resource_url}" title="{resource_title}" brief="{resource_brief}" color="{current_color}" %}}\n')
-            current_color = color_two if current_color == color_one else color_one
+            f.write(f'{{% include freelearningcontent.html url="{resource_url}" title="{resource_title}" brief="{resource_brief}" %}}\n')
